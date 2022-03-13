33d3f23f32d5780e941e4315ddad6a0dff4222eb
Devitar <devin.curtis1210@gmail.com>
2022-03-12 17:01:39 -0700

    Bot can read message reactions and determine number of unique reactions

--- main.js
+++ main.js
@@ -18,9 +18,28 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", async msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong")
+client.on('messageReactionAdd', async reaction => {
+    if (reaction.partial) {
+        try {
+            await reaction.fetch()
+        } catch (error) {
+            console.error('Something went wrong when fetching the message:', error)
+
+            return
+        }
+    }
+
+    const msg = reaction.message
+    const cache = msg.reactions.cache
+    const uniqueReactions = cache.reduce((prev, current) => {
+        const userWhoReactedId = current.client.user.id
+        return !prev.userIds.includes(userWhoReactedId)
+            ? { userIds: [...prev.userIds, userWhoReactedId], total: prev.total + 1 }
+            : prev
+    }, { userIds: [], total: 0 })
+
+    if (uniqueReactions.total >= 5) {
+        // Copy msg.attachments and move them to "best of" channel
     }
 })
 
